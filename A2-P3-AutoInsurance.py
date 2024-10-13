6ebdd1f4726783394a0d383047a19c29b6a6122a
Zeddyare <w0433704@nscc.ca>
2024-10-12 22:52:52 -0300

    Working and matching examples, made it inclusive ;)

--- A2-P3-AutoInsurance.py
+++ A2-P3-AutoInsurance.py
@@ -1,11 +1,46 @@
 #Program 3 – Auto Insurance
 #Write a program that computes monthly insurance according to the provided schedule. 
 
-#Student #:     
-#Student Name:  
+#Student #: 0433704
+#Student Name: Zachary Rudolf
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+
+    #Variable initialize
+    sex=""
+    price=0.0
+    age=0
+    rate=0.0
+    monthlyPay=0.0
+
+    #Welcome message
+    print("Welcome to Scooter's Insurance!\n")
+
+    #Input for vehicle price, sex, and age
+    sex=input("Are you 'Male' or 'Female'?: ").lower()
+    age=int(input("\nEnter your age: "))
+    price=float(input("\nEnter the purchase price of the vehicle: $"))
+    #if statements to find rate based on inputs
+    if sex=="male" or sex=="m":
+        if age >=15 and age <25:
+            rate=0.25
+        elif age >=25 and age <40:
+            rate=0.17
+        elif age >=40 and age <70:
+            rate=0.10
+    elif sex=="female" or sex=="f":
+        if age >=15 and age <25:
+            rate=0.20
+        elif age >=25 and age <40:
+            rate=0.15
+        elif age >=40 and age <70:
+            rate=0.10
+    else: 
+        print("No charge for those outside the binary.") 
+    monthlyPay=(price*rate)/12 
+    #print output for final price
+    print("\nYour monthly insurance will be: ${0:,.2f}".format(monthlyPay))
 
 
 
